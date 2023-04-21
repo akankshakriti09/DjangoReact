a0c5060828368971c1b52f28b7ec584d96b22888
Akanksha Kriti <akankshakriti09@gmail.com>
2023-04-21 23:25:20 +0530

    commited

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -5,10 +5,14 @@ from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 # Create your views here.
-class RoomView(generics.ListAPIView):
+
+
+class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
@@ -27,11 +31,11 @@ class CreateRoomView(APIView):
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
-                room.save(update_fields=['guest_can_pause' , 'votes_to_skip'])
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
-                room = Room(host=host, guest_can_pause =guest_can_pause, votes_to_skip = votes_to_skip)
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
-            
+
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
