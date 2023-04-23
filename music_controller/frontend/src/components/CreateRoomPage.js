8f007d90d01acd2ebe6b8b48304f3a81e36d8233
Akanksha Kriti <akankshakriti09@gmail.com>
2023-04-23 17:46:02 +0530

    added Grid in CreateRoomPage

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,10 +1,31 @@
 import React, {Component} from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/core/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
  export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
+
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <p>This is Create Room Page.</p>;    
+        return ( 
+          <Grid container spacing={1}>
+             <Grid item xs={120} align="center">
+                <Typography component="h4" variant="h4">
+                    Create A Room
+                </Typography>
+             </Grid>
+          </Grid>
+        );    
     }
  }
